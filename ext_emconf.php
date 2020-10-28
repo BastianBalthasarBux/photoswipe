f3634f12a95277ea373cf69a90dad9c155dc032a
Tobias Eichelberger <tobias.eichelberger@dacoco.de>
2020-10-28 18:19:00 +0100

    TER description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Photoswipe',
-    'description' => 'Photoswipe image zoom',
+    'description' => 'Enables Photoswipe for TYPO3\'s native image enlargement. Small js-lib and easy to install. It just works with plain javascript, no jQuery or other frameworks are needed.',
     'author' => 'Tobi Eichelberger',
     'author_company' => '',
     'author_email' => 'tobi.eichelberger@gmail.com',
