4dadaa379d2f14bd68a5bb871f10cb300e1107da
Romain Tiger <romain.tiger@cameros.fr>
2023-09-08 16:26:55 +0200

    [TASK] Typo3v12 compatibility

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') || defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('photoswipe', 'Configuration/TypoScript', 'PhotoSwipe');
